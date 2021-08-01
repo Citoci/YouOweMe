ddef6b88a2a0cc883f4c1256a2c71be32299ad28
Claudio Cito <claudiocito1997@gmail.com>
2021-08-01 23:24:48 +0200

    Contact Balances List in Jetpack Compose

--- app/src/main/java/com/cito/youoweme/ContactBalancesListFragment.kt
+++ app/src/main/java/com/cito/youoweme/ContactBalancesListFragment.kt
@@ -5,6 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,30 +27,87 @@ import com.cito.youoweme.data.model.Transaction
 import com.cito.youoweme.data.sql_database.ContactsSQLiteDAO
 import com.cito.youoweme.data.sql_database.TransactionsSQLiteDAO
 import com.cito.youoweme.databinding.ContactBalanceEntryBinding
+import com.cito.youoweme.ui.theme.YouOweMeTheme
 
+// NOW WITH JETPACK COMPOSE!!
 class ContactBalancesListFragment : Fragment() {
 
+    private var contacts = listOf<Contact>()
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_contact_balances_list, container, false)
+        updateContacts()
+        return ComposeView(requireContext()).apply {
+            setContent {
+                YouOweMeTheme {
+                    PerContactBalancesList(contacts)
+                }
+            }
+        }
+//        return inflater.inflate(R.layout.fragment_contact_balances_list, container, false)
     }
 
     override fun onResume() {
         super.onResume()
-        with(activity?.findViewById<RecyclerView>(R.id.contact_balances_list)) {
-            this?.layoutManager = LinearLayoutManager(context)
-            this?.adapter = ContactBalancesRecicleViewAdapter(
-                ContactsSQLiteDAO.getAll()?.apply {
-                    TransactionsSQLiteDAO.getAll()?.let { transactions ->
-                        this.forEach { contact ->
-                            contact.calculateBalance(transactions)
-                        }
-                    }
-                } ?: listOf()
-            )
+        updateContacts()
+    }
+
+    @Composable
+    fun PerContactBalancesList(contacts: List<Contact>) {
+        LazyColumn {
+            items(
+                items = contacts,
+                key = { it.id ?: -1 }
+            ) {
+                ContactBalance(it)
+            }
         }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun ContactBalance(contact: Contact = Contact(null, "Mario", "Rossi", 5f)) {
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .clickable { }
+            .padding(8.dp)) {
+            Text(text = contact.name ?: "")
+            Text(text = contact.surname ?: "",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 8.dp))
+            Text(text = "" + (contact.balance ?: 0))
+        }
+    }
+
+//    override fun onResume() {
+//        super.onResume()
+//        with(activity?.findViewById<RecyclerView>(R.id.contact_balances_list)) {
+//            this?.layoutManager = LinearLayoutManager(context)
+//            this?.adapter = ContactBalancesRecycleViewAdapter(
+//                ContactsSQLiteDAO.getAll()?.apply {
+//                    TransactionsSQLiteDAO.getAll()?.let { transactions ->
+//                        this.forEach { contact ->
+//                            contact.calculateBalance(transactions)
+//                        }
+//                    }
+//                } ?: listOf()
+//            )
+//        }
+//    }
+
+    private fun updateContacts() {
+        contacts = ContactsSQLiteDAO.getAll()?.apply {
+            TransactionsSQLiteDAO.getAll()?.let { transactions ->
+                this.forEach { contact ->
+                    contact.calculateBalance(transactions)
+                }
+            }
+        } ?: listOf()
     }
 
     private fun Contact.calculateBalance(transactions: List<Transaction>): Float {
@@ -46,30 +117,30 @@ class ContactBalancesListFragment : Fragment() {
         return sum.also { balance = it }
     }
 
-    inner class ContactBalancesRecicleViewAdapter(private val values: List<Contact>) :
-        RecyclerView.Adapter<ContactBalancesRecicleViewAdapter.ContactViewHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-            return ContactViewHolder(
-                ContactBalanceEntryBinding.inflate(
-                    LayoutInflater.from(parent.context), parent, false
-                )
-            )
-        }
-
-        override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-            val item = values[position]
-
-            holder.contentView.text = "$item : ${item.balance}"
-        }
-
-        override fun getItemCount(): Int = values.size
-
-        inner class ContactViewHolder(binding: ContactBalanceEntryBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-            val contentView: TextView = binding.txtfghty
-        }
-
-    }
+//    inner class ContactBalancesRecycleViewAdapter(private val values: List<Contact>) :
+//        RecyclerView.Adapter<ContactBalancesRecycleViewAdapter.ContactViewHolder>() {
+//
+//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+//            return ContactViewHolder(
+//                ContactBalanceEntryBinding.inflate(
+//                    LayoutInflater.from(parent.context), parent, false
+//                )
+//            )
+//        }
+//
+//        override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+//            val item = values[position]
+//
+//            holder.contentView.text = "$item : ${item.balance}"
+//        }
+//
+//        override fun getItemCount(): Int = values.size
+//
+//        inner class ContactViewHolder(binding: ContactBalanceEntryBinding) :
+//            RecyclerView.ViewHolder(binding.root) {
+//            val contentView: TextView = binding.txtfghty
+//        }
+//
+//    }
 
 }
--- app/src/main/java/com/cito/youoweme/MainActivity.kt
+++ app/src/main/java/com/cito/youoweme/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.cito.youoweme
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
-import android.view.View
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.cito.youoweme.data.model.Transaction
 import com.cito.youoweme.data.sql_database.ContactsSQLiteDAO
 import com.cito.youoweme.data.sql_database.TransactionsSQLiteDAO
 import com.cito.youoweme.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
